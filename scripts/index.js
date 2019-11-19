8d00947a0c834ce15360c384c102ddf44ca7e6c6
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-11-19 20:30:46 +0500

    getting clicks and puting symbols into cells

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,58 @@
-let Gameboard = {
-    '0': '',
-    '1': '',
-    '2': '',
-    '3': '',
-    '4': '',
-    '5': '',
-    '6': '',
-    '7': '',
-    '8': '',
-}
+// let Gameboard = {
+//     0: '',
+//     1: '',
+//     2: '',
+//     3: 'O',
+//     4: '',
+//     5: '',
+//     6: '',
+//     7: '',
+//     8: 'x',
+// }
+let Gameboard = []
 
-function render_board(Gameboard) {
-    let cells = document.getElementsByClassName('cell');
+const player1 = 'X';
+const player2 = 'O';
 
-    for (let key in Gameboard) {
-        let i = parseInt(key);
-        cells[i].innerText = Gameboard[key];
+const gameBoard = (() => {
+
+    const cells = document.querySelectorAll('.cell');
+
+    // function game() {
+    //     document.querySelector(".restart").style.display = "none";
+
+    // }
+
+    function render_board(Gameboard) {
+        //let cells = document.getElementsByClassName('cell');
+
+        Gameboard = Array.from(Array(8).keys());
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].innerText = '';
+            // cells[i].style.removeProperty('background-color');
+            cells[i].addEventListener('click', clicks, false);
+        }
     }
-    
-}
 
+    function clicks(cell) {
+        turn(cell.target.id, player1);
+        console.log(cell.target.id);
+    }
 
-render_board(Gameboard)
+    function turn(cellId, player) {
+
+        Gameboard[cellId] = player;
+        document.getElementById(cellId).innerText = player;
+
+    }
+    return render_board;
+
+})();
+
+gameBoard(Gameboard);
+
+const Player = (player, number) => {
+
+    return { player, number }
+
+};
