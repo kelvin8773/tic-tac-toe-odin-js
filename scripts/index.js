e16a9aafa1ecd8d10a3c28d60f29f3df0ed0ca38
Kelvin Liang <kelvin9877@gmail.com>
2019-12-02 22:35:28 +0800

    finish adding scores to the game features

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 const DataModule = (() => {
   const Player = (name, symbol) => {
-    const selfName = name;
-    const selfSymbol = symbol;
     let score = 0;
-    const getName = () => selfName;
-    const getSymbol = () => selfSymbol;
+    let myName = name;
+
+    const getName = () => myName;
+    const setName = (name) => myName = name;
+    const getSymbol = () => symbol;
+
     const getScore = () => score;
     const addScore = () => {
       score += 1;
@@ -12,6 +14,7 @@ const DataModule = (() => {
 
     return {
       getName,
+      setName,
       getSymbol,
       getScore,
       addScore,
@@ -108,10 +111,11 @@ const DataModule = (() => {
 const UIModule = (() => {
   const DOMSelectors = {
     startButton: '#btn-start',
-    nextButton: '#btn-next',
-    board: '#gameboard',
-    allcells: '.cell',
+    resetButton: '#btn-reset',
+    board: '#gameBoard',
+    allCells: '.cell',
     message: '#message-line',
+    gameHelper: '#gameHelper',
   };
   const startBtn = document.querySelector(DOMSelectors.startButton);
 
@@ -160,16 +164,16 @@ const UIModule = (() => {
   const updateStartButton = (status = '') => {
     switch (status) {
       case 'start':
-        startBtn.innerText = 'Reset';
-        startBtn.className = 'btn btn-danger btn-lg';
+        startBtn.innerText = 'Restart';
+        startBtn.className = 'btn btn-warning btn-lg btn-block';
         break;
       case 'finish':
         startBtn.innerText = 'Play Again!';
-        startBtn.className = 'btn btn-info btn-block';
+        startBtn.className = 'btn btn-success btn-lg btn-block';
         break;
       default:
         startBtn.innerText = 'Start';
-        startBtn.className = 'btn btn-primary btn-lg';
+        startBtn.className = 'btn btn-primary btn-lg btn-block';
     }
   };
 
@@ -181,7 +185,7 @@ const UIModule = (() => {
   };
 
   const clearBoard = () => {
-    const cells = document.querySelectorAll(DOMSelectors.allcells);
+    const cells = document.querySelectorAll(DOMSelectors.allCells);
 
     cells.forEach(c => {
       c.innerText = '';
@@ -205,72 +209,67 @@ const UIModule = (() => {
 
 const Controller = ((Data, UI) => {
   const DOM = UI.getDOMSelectors();
-  let player1 = Data.Player(UI.getPlayersName('player1'), 'X');
-  let player2 = Data.Player(UI.getPlayersName('player2'), 'O');
-  let game = Data.Game(Data.Board(), player1, player2);
-  console.log(UI.getPlayersName('player1'));
 
-  const boardNode = document.querySelector(DOM.board);
-
-  const runGame = event => {
-    UI.showMessage(`${game.getNextPlayer().getName()}, It is your Turn.`);
-    UI.updateStartButton('start');
-    const clickedCell = event.target.id;
-
-    if (clickedCell === undefined) return;
-
-    const mark = game.turn(clickedCell);
-
-    if (mark !== undefined) {
-      UI.markPosition(mark);
-
-      if (game.isGameOver()) {
-        const winner = game.getWinner();
-        if (winner) {
-          UI.showWinCombo(game.getWinCombo());
-          winner.addScore();
-          UI.updatePlayerScore(winner);
-          UI.showMessage(`Congratulation! ${winner.getName()} won!`);
-        } else {
-          UI.showMessage('The Board is full, please try again!');
-        }
-        boardNode.removeEventListener('click', runGame);
-        UI.updateStartButton('finish');
-      }
-      game.switchPlayer();
-    }
-  };
+  const player1 = Data.Player(UI.getPlayersName('player1'), 'X');
+  const player2 = Data.Player(UI.getPlayersName('player2'), 'O');
+  let gameSwitch = false;
 
   const resetGame = () => {
-    player1 = Data.Player(UI.getPlayersName('player1'), 'X');
-    player2 = Data.Player(UI.getPlayersName('player2'), 'O');
-
-    game = Data.Game(Data.Board(), player1, player2);
-
-    UI.clearBoard();
-    UI.updateStartButton();
-    UI.updatePlayersName();
-
-    UI.updatePlayerScore(player1);
-    UI.updatePlayerScore(player2);
-
-    UI.showMessage(`Game Started, ${UI.getPlayersName()}, You Go First!`);
-
-    boardNode.addEventListener('click', runGame);
+    window.location.reload(true);
   };
 
   const startGame = () => {
-    game = Data.Game(Data.Board(), player1, player2);
+    const name1 = UI.getPlayersName('player1');
+    const name2 = UI.getPlayersName('player2');
+
+    if (name1 !== 'Player1') { player1.setName(name1) };
+    if (name2 !== 'Player2') { player2.setName(name2) };
 
     UI.clearBoard();
-    UI.updateStartButton();
 
+    const game = Data.Game(Data.Board(), player1, player2);
+    const boardNode = document.querySelector(DOM.board);
+
+    if (gameSwitch) {
+      game.switchPlayer();
+      UI.showMessage(`Play Again! Now ~ ${game.getActivePlayer().getName()} ~ First!!`);
+      gameSwitch = false;
+    } else {
+      UI.showMessage(`Game Started! ~ ${game.getActivePlayer().getName()} ~, You First!`);
+      gameSwitch = true;
+    };
+
+    UI.updateStartButton('start')
     UI.updatePlayersName();
 
-    UI.updatePlayerScore(player1);
-    UI.updatePlayerScore(player2);
+    const runGame = event => {
+      const clickedCell = event.target.id;
+      const clickedCellValue = event.target.innerText;
+      if (clickedCell === undefined || clickedCellValue != '') return;
 
-    UI.showMessage('Game Started!');
+      UI.showMessage(` ~ ${game.getNextPlayer().getName()} ~, you are Next!!!`);
+
+      const mark = game.turn(clickedCell);
+
+      if (mark !== undefined) {
+        UI.markPosition(mark);
+
+        if (game.isGameOver()) {
+          const winner = game.getWinner();
+          if (winner) {
+            UI.showWinCombo(game.getWinCombo());
+            winner.addScore();
+            UI.updatePlayerScore(winner);
+            UI.showMessage(`Congratulation! ${winner.getName()} won!`);
+          } else {
+            UI.showMessage('The Board is full, please try again!');
+          }
+          UI.updateStartButton('finish');
+          boardNode.removeEventListener('click', runGame);
+        }
+        game.switchPlayer();
+      }
+    };
 
     boardNode.addEventListener('click', runGame);
   };
@@ -279,7 +278,7 @@ const Controller = ((Data, UI) => {
     document
       .querySelector(DOM.startButton)
       .addEventListener('click', startGame);
-    document.querySelector(DOM.nextButton).addEventListener('click', resetGame);
+    document.querySelector(DOM.resetButton).addEventListener('click', resetGame);
   };
 
   return { init };
